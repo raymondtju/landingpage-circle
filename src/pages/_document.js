83d964f44970542ed7d19c5430250c6c73eefb20
raymondtju <raymond.rtju@gmail.com>
2023-02-06 23:21:12 +0700

    document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,10 +8,14 @@ export default function Document() {
       <body className="text-white bg-background">
         <Main />
         <NextScript />
-        <Script
+        {/* <Script
+          src="./node_modules/flowbite/dist/flowbite.min.js"
+          strategy="beforeInteractive"
+        ></Script> */}
+        {/* <Script
           src="/node_modules/flowbite/dist/flowbite.min.js"
           strategy="beforeInteractive"
-        ></Script>
+        /> */}
 
         {/* <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></Script> */}
       </body>
