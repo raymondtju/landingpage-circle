c2bcb86166afcc5fba14027f2ca0d25eb6fe2485
raymondtju <raymond.rtju@gmail.com>
2023-02-06 23:16:50 +0700

    Revert "Revert "components and assets""
    This reverts commit 6abc81dcb1b9f01bab599a318bc353ca530a771b.

--- src/components/TestimonialCard/index.jsx
+++ src/components/TestimonialCard/index.jsx
@@ -14,7 +14,7 @@ export default function TestimonialCard({ author, role, message }) {
       <div
         className={`w-[470px] px-10 m-auto pt-5 py-8 bg-[#151925] rounded-xl ring-[#151925] hover:ring-2 hover:ring-[#0ED2F7] duration-300 transition-all ease-in-out`}
       >
-        <div class="flex items-center gap-4">
+        <div className="flex items-center gap-4">
           <div className="block w-10 h-10 bg-white rounded-full"></div>
           <div className="flex flex-col">
             <strong>{author}</strong>
--- src/components/TestimonialSection/index.jsx
+++ src/components/TestimonialSection/index.jsx
@@ -7,7 +7,7 @@ export default function TestimonialSection() {
       {/* Section 6 */}
       <div className="mt-[200px]">
         <div className="mx-auto text-center">
-          <h3 className="text-5xl leading-[62px] font-bold">
+          <h3 className="text-4xl lg:text-5xl leading-[42px] lg:leading-[62px] font-bold">
             What people are saying about
             <span className="textGradient">{` Circle`}</span>
           </h3>
